90ad64e19172b5a1782bb30929853eb7d5ebf26a
Thomas <thomas.borstad@gmail.com>
2016-01-11 10:34:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ YO_API_TOKEN='YOUR_API_TOKEN'
 URLS = [
 'http://nettbutikk.netcom.no/mobil/apple/iphone-6s-plus/128gb-stellargr%C3%A5',
 'http://nettbutikk.netcom.no/mobil/apple/iphone-6s-plus/128gb-s%C3%B8lv']
-DIVCLASS_STRING = [('stock-status','Forventet leveringstid 3-4 uker')]
+DIVCLASS_STRING = [('stock-status','Forventet leveringstid 2-3 uker')]
